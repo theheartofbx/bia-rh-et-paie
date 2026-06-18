c72d83fdf30210f6bba3a75e786469face9818bb
hilarionntankoh <hilarionntankoh@groupebia.com>
2026-06-18 22:44:57 +0100

    refonte: extraction org.sitracel.discipline.model en bundle autonome
    - Nouveau bundle Discipline_Conge/org.sitracel.discipline.model
    - Factory complète : 7 classes discipline enregistrées
    - Classes déplacées depuis org.sitracel.model (méga-bundle)
    - MANIFEST méga-bundle mis à jour : discipline.model retiré des exports

--- Discipline_Conge/org.sitracel.discipline.model/src/org/sitracel/discipline/model/factory/SitracelDisciplineModelFactory.java
+++ Discipline_Conge/org.sitracel.discipline.model/src/org/sitracel/discipline/model/factory/SitracelDisciplineModelFactory.java
@@ -5,6 +5,13 @@ import java.sql.ResultSet;
 import org.adempiere.base.IModelFactory;
 import org.compiere.model.PO;
 import org.compiere.util.Env;
+import org.sitracel.discipline.model.I_HR_Delai_Reponse;
+import org.sitracel.discipline.model.I_HR_Demande_Explication;
+import org.sitracel.discipline.model.I_HR_Dossier_Disciplinaire;
+import org.sitracel.discipline.model.I_HR_Duree_Sanction;
+import org.sitracel.discipline.model.I_HR_Punishment;
+import org.sitracel.discipline.model.I_HR_Sanction_Autorisation;
+import org.sitracel.discipline.model.I_HR_TypeSanction;
 import org.sitracel.discipline.model.MHRDelaiReponse;
 import org.sitracel.discipline.model.MHRDemandeExplication;
 import org.sitracel.discipline.model.MHRDossierDisciplinaire;
@@ -13,24 +20,62 @@ import org.sitracel.discipline.model.MHRPunishment;
 import org.sitracel.discipline.model.MHRSanctionAutorisation;
 import org.sitracel.discipline.model.MHRTypeSanction;
 
-public class SitracelDisciplineModelFactory implements IModelFactory{
+public class SitracelDisciplineModelFactory implements IModelFactory {
 
-	@Override
-	public Class<?> getClass(String tableName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Class<?> getClass(String tableName) {
+        if (tableName.equalsIgnoreCase(I_HR_Delai_Reponse.Table_Name))
+            return MHRDelaiReponse.class;
+        if (tableName.equalsIgnoreCase(I_HR_Demande_Explication.Table_Name))
+            return MHRDemandeExplication.class;
+        if (tableName.equalsIgnoreCase(I_HR_Dossier_Disciplinaire.Table_Name))
+            return MHRDossierDisciplinaire.class;
+        if (tableName.equalsIgnoreCase(I_HR_Duree_Sanction.Table_Name))
+            return MHRDureeSanction.class;
+        if (tableName.equalsIgnoreCase(I_HR_Punishment.Table_Name))
+            return MHRPunishment.class;
+        if (tableName.equalsIgnoreCase(I_HR_Sanction_Autorisation.Table_Name))
+            return MHRSanctionAutorisation.class;
+        if (tableName.equalsIgnoreCase(I_HR_TypeSanction.Table_Name))
+            return MHRTypeSanction.class;
+        return null;
+    }
 
-	@Override
-	public PO getPO(String tableName, int Record_ID, String trxName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public PO getPO(String tableName, int Record_ID, String trxName) {
+        if (tableName.equalsIgnoreCase(I_HR_Delai_Reponse.Table_Name))
+            return new MHRDelaiReponse(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Demande_Explication.Table_Name))
+            return new MHRDemandeExplication(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Dossier_Disciplinaire.Table_Name))
+            return new MHRDossierDisciplinaire(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Duree_Sanction.Table_Name))
+            return new MHRDureeSanction(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Punishment.Table_Name))
+            return new MHRPunishment(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Sanction_Autorisation.Table_Name))
+            return new MHRSanctionAutorisation(Env.getCtx(), Record_ID, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_TypeSanction.Table_Name))
+            return new MHRTypeSanction(Env.getCtx(), Record_ID, trxName);
+        return null;
+    }
 
-	@Override
-	public PO getPO(String tableName, ResultSet rs, String trxName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    @Override
+    public PO getPO(String tableName, ResultSet rs, String trxName) {
+        if (tableName.equalsIgnoreCase(I_HR_Delai_Reponse.Table_Name))
+            return new MHRDelaiReponse(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Demande_Explication.Table_Name))
+            return new MHRDemandeExplication(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Dossier_Disciplinaire.Table_Name))
+            return new MHRDossierDisciplinaire(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Duree_Sanction.Table_Name))
+            return new MHRDureeSanction(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Punishment.Table_Name))
+            return new MHRPunishment(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_Sanction_Autorisation.Table_Name))
+            return new MHRSanctionAutorisation(Env.getCtx(), rs, trxName);
+        if (tableName.equalsIgnoreCase(I_HR_TypeSanction.Table_Name))
+            return new MHRTypeSanction(Env.getCtx(), rs, trxName);
+        return null;
+    }
 }
