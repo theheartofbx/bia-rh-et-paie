74dcae90c1cd14952d4176355df0f08e6fb9191d
hilarionntankoh <hilarionntankoh@groupebia.com>
2026-07-08 14:53:44 +0100

    Session 9 (suite) : suppression du process obsolete SitracelProcessNotifierConge (notifications gerees automatiquement par le modelvalidator depuis longtemps), champ 'Notifier' desactive dans la fenetre Conge, AD_Process desactive

--- Discipline_Conge/org.sitracel.conge.process/src/org/sitracel/conge/process/factory/SitracelCongeProcessFactory.java
+++ Discipline_Conge/org.sitracel.conge.process/src/org/sitracel/conge/process/factory/SitracelCongeProcessFactory.java
@@ -5,36 +5,30 @@ import org.compiere.process.ProcessCall;
 import org.sitracel.conge.process.conge.SitracelProcessActualiserAbsence;
 import org.sitracel.conge.process.conge.SitracelProcessApprouverConge;
 import org.sitracel.conge.process.conge.SitracelProcessDesapprouverConge;
-import org.sitracel.conge.process.conge.SitracelProcessNotifierConge;
 import org.sitracel.conge.process.conge.SitracelProcessRejeterConge;
 import org.sitracel.conge.process.conge.SitracelProcessValiderConge;
 
-public class SitracelCongeProcessFactory implements IProcessFactory{
+public class SitracelCongeProcessFactory implements IProcessFactory {
 
-	@Override
-	public ProcessCall newProcessInstance(String className) {
-		// TODO Auto-generated method stub
-		
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessActualiserAbsence")) {
-			return new SitracelProcessActualiserAbsence();
-		}
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessApprouverConge")) {
-			return new SitracelProcessApprouverConge();
-		}
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessDesapprouverConge")) {
-			return new SitracelProcessDesapprouverConge();
-		}
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessValiderConge")) {
-			return new SitracelProcessValiderConge();
-		}
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessRejeterConge")) {
-			return new SitracelProcessRejeterConge();
-		}
-		if(className.equals("org.sitracel.conge.process.conge.SitracelProcessNotifierConge")) {
-			return new SitracelProcessNotifierConge();
-		}
-		
-		return null;
-	}
+    @Override
+    public ProcessCall newProcessInstance(String className) {
 
+        if (className.equals("org.sitracel.conge.process.conge.SitracelProcessActualiserAbsence")) {
+            return new SitracelProcessActualiserAbsence();
+        }
+        if (className.equals("org.sitracel.conge.process.conge.SitracelProcessApprouverConge")) {
+            return new SitracelProcessApprouverConge();
+        }
+        if (className.equals("org.sitracel.conge.process.conge.SitracelProcessDesapprouverConge")) {
+            return new SitracelProcessDesapprouverConge();
+        }
+        if (className.equals("org.sitracel.conge.process.conge.SitracelProcessValiderConge")) {
+            return new SitracelProcessValiderConge();
+        }
+        if (className.equals("org.sitracel.conge.process.conge.SitracelProcessRejeterConge")) {
+            return new SitracelProcessRejeterConge();
+        }
+
+        return null;
+    }
 }
